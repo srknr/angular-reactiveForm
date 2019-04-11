ef85abcaa024ab442fcad9d32d1c6d34ec274824
srknr <k_srkn13@yahoo.co.jp>
2019-04-11 16:18:21 +0900

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, NameEditorComponent, ProfileEditorComponent ],
+  declarations: [ AppComponent, ProfileEditorComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
